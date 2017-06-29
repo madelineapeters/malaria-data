e4fa831626a863e53d7dc3f4edbf2624368c21b8
Madeline <madelineapeters@gmail.com>
2017-06-29 15:38:25 -0400

    Add files via upload

--- Mal bootstrap FPP.R
+++ Mal bootstrap FPP.R
@@ -1,7 +1,7 @@
 #select time points to compare
 
-#bootstrap to find false positive rate for time 0
-bootobject.2<-replicate(10000, {
+#bootstrap to find CI for time 0
+bootobject.2<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
@@ -10,7 +10,7 @@ bootobject.2<-replicate(10000, {
   
   for (i in x){
     #sample replicate
-    dif.start2<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,i]))
+    dif.start2<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=mal_sub[1:hap_num,i]))
     #edit dataframe
     names(dif.start2)<-c("Hap")
     dif.start2<-count(dif.start2,Hap)
@@ -27,105 +27,27 @@ bootobject.2<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
-  dif<-dif.mid$R1-dif.mid$R2
+  dif<-dif.mid$R2-dif.mid$R2
 }
 )
 
 med.T0<-median(bootobject.2)
 boot.df[hap,10]<-med.T0
 
-bootobject.1<-replicate(10000, {
-
-  dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
-  names(dif.df)<-c("Hap")
-  dif.df[1:hap_num,]<-c(1:hap_num)
-  dif.mid<-dif.df
-  
-    for (i in x){
-      #sample replicate
-      dif.start<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,sample(x, size=1, replace=TRUE)]))
-      #edit dataframe
-      names(dif.start)<-c("Hap")
-      dif.start<-count(dif.start,Hap)
-      #join counts with Hap labels
-      dif.mid<-full_join(dif.mid, dif.start, by="Hap")
-      #replace NAs
-      dif.mid[is.na(dif.mid)]<-0
-    }
-    
-    #rename replicate columns
-    names(dif.mid)[2:3]<-c("R1","R2")
-    
-    #select haplotype
-    dif.mid<-slice(dif.mid, hap)
-    
-    #transform to proportions
-    dif.mid[,2:3]<-dif.mid[,2:3]/10000
-    
-    #add difference column
-    dif<-dif.mid$R1-dif.mid$R2
-  }
-)
-bootobject.1<-sort(bootobject.1, decreasing=FALSE)
-boot.lower<-length(bootobject.1[bootobject.1 < med.T0])
-boot.df[hap,8]<-boot.lower
-boot.upper<-length(bootobject.1[bootobject.1 < med.T0])
-boot.df[hap,9]<-boot.upper
-boot.CI<-c(boot.lower, boot.upper)
-boot.CI<-min(boot.CI)
-CI.T0<-1-(boot.CI/10000)
-
-
-#bootstrap to find false positive rate for time 48
-bootobject.2<-replicate(10000, {
+avg.df<-as.data.frame((mal_sub[,2]+mal_sub[,6])/2)
+bootobject.1<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
   dif.df[1:hap_num,]<-c(1:hap_num)
   dif.mid<-dif.df
   
-  for (i in w){
+  for (i in x){
     #sample replicate
-    dif.start2<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,i]))
-    #edit dataframe
-    names(dif.start2)<-c("Hap")
-    dif.start2<-count(dif.start2,Hap)
-    #join counts with Hap labels
-    dif.mid<-full_join(dif.mid, dif.start2, by="Hap")
-    #replace NAs
-    dif.mid[is.na(dif.mid)]<-0
-  }
-  
-  #rename replicate columns
-  names(dif.mid)[2:3]<-c("R1","R2")
-  
-  #select haplotype
-  dif.mid<-slice(dif.mid, hap)
-  
-  #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
-  
-  #add difference column
-  dif<-dif.mid$R1-dif.mid$R2
-}
-)
-
-med.T48<-median(bootobject.2)
-boot.df[hap,13]<-med.T48
-
-bootobject.1<-replicate(10000, {
-  
-  dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
-  names(dif.df)<-c("Hap")
-  dif.df[1:hap_num,]<-c(1:hap_num)
-  dif.mid<-dif.df
-  
-  for (i in w){
-    #sample replicate
-    dif.start<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,sample(w, size=1, replace=TRUE)]))
+    dif.start<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=avg.df[,1]))
     #edit dataframe
     names(dif.start)<-c("Hap")
     dif.start<-count(dif.start,Hap)
@@ -142,19 +64,99 @@ bootobject.1<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
-  dif<-dif.mid$R1-dif.mid$R2
+  dif<-dif.mid$R2-dif.mid$R1
 }
 )
 bootobject.1<-sort(bootobject.1, decreasing=FALSE)
-boot.lower<-length(bootobject.1[bootobject.1 < med.T48])
+boot.lower<-(length(bootobject.1[(bootobject.1) < (med.T0)]))
+boot.df[hap,8]<-boot.lower
+boot.upper<-(length(bootobject.1[(bootobject.1) > (med.T0)]))
+boot.df[hap,9]<-boot.upper
+boot.CI<-c(boot.lower, boot.upper)
+boot.CI<-min(boot.CI)
+CI.T0<-1-(boot.CI/1000)
+
+
+#bootstrap to find CI for time 48
+bootobject.2<-replicate(1000, {
+  
+  dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
+  names(dif.df)<-c("Hap")
+  dif.df[1:hap_num,]<-c(1:hap_num)
+  dif.mid<-dif.df
+  
+  for (i in w){
+    #sample replicate
+    dif.start2<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=mal_sub[,i]))
+    #edit dataframe
+    names(dif.start2)<-c("Hap")
+    dif.start2<-count(dif.start2,Hap)
+    #join counts with Hap labels
+    dif.mid<-full_join(dif.mid, dif.start2, by="Hap")
+    #replace NAs
+    dif.mid[is.na(dif.mid)]<-0
+  }
+  
+  #rename replicate columns
+  names(dif.mid)[2:3]<-c("R1","R2")
+  
+  #select haplotype
+  dif.mid<-slice(dif.mid, hap)
+  
+  #transform to proportions
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
+  
+  #add difference column
+  dif<-dif.mid$R2-dif.mid$R1
+}
+)
+
+med.T48<-median(bootobject.2)
+boot.df[hap,13]<-med.T48
+
+avg.df<-as.data.frame((mal_sub[,4]+mal_sub[,8])/2)
+bootobject.1<-replicate(1000, {
+  
+  dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
+  names(dif.df)<-c("Hap")
+  dif.df[1:hap_num,]<-c(1:hap_num)
+  dif.mid<-dif.df
+  
+  for (i in w){
+    #sample replicate
+    dif.start<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=avg.df[,1]))
+    #edit dataframe
+    names(dif.start)<-c("Hap")
+    dif.start<-count(dif.start,Hap)
+    #join counts with Hap labels
+    dif.mid<-full_join(dif.mid, dif.start, by="Hap")
+    #replace NAs
+    dif.mid[is.na(dif.mid)]<-0
+  }
+  
+  #rename replicate columns
+  names(dif.mid)[2:3]<-c("R1","R2")
+  
+  #select haplotype
+  dif.mid<-slice(dif.mid, hap)
+  
+  #transform to proportions
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
+  
+  #add difference column
+  dif<-dif.mid$R2-dif.mid$R1
+}
+)
+bootobject.1<-sort(bootobject.1, decreasing=FALSE)
+boot.lower<-(length(bootobject.1[(bootobject.1) < (med.T48)]))
 boot.df[hap,11]<-boot.lower
-boot.upper<-length(bootobject.1[bootobject.1 < med.T48])
+boot.upper<-(length(bootobject.1[(bootobject.1) > (med.T48)]))
 boot.df[hap,12]<-boot.upper
 boot.CI<-c(boot.lower, boot.upper)
 boot.CI<-min(boot.CI)
-CI.T48<-1-(boot.CI/10000)
+CI.T48<-1-(boot.CI/1000)
 
 CI<-max(CI.T0, CI.T48)
--- Mal bootstrap med.R
+++ Mal bootstrap med.R
@@ -1,5 +1,10 @@
+#set CI based on time comparisons
+if (CI > 0.95) {CI <- CI} else if (CI < 0.95) {CI <- 0.95}
+CI.2<-0.9
+
 #bootstrap to CI and median for replicate 1
-bootobject.1<-replicate(10000, {
+avg.df<-as.data.frame((mal_sub[,2]+mal_sub[,4])/2)
+bootobject.1<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
@@ -8,7 +13,7 @@ bootobject.1<-replicate(10000, {
   
   for (i in y){
     #sample replicate
-    dif.start<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,sample(y, size=1, replace=TRUE)]))
+    dif.start<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=avg.df[,1]))
     #edit dataframe
     names(dif.start)<-c("Hap")
     dif.start<-count(dif.start,Hap)
@@ -25,19 +30,23 @@ bootobject.1<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
-  dif<-dif.mid$R1-dif.mid$R2
+  dif<-dif.mid$R2-dif.mid$R1
 }
 )
+
 bootobject.1<-sort(bootobject.1, decreasing=FALSE)
-boot.lower<-nth(bootobject.1, 500)
+boot.lower<-nth(bootobject.1, (1000*(1-CI)))
 boot.df[hap,3]<-boot.lower
-boot.upper<-nth(bootobject.1, 9500)
+boot.upper<-nth(bootobject.1, (1000*CI))
 boot.df[hap,4]<-boot.upper
 
-bootobject.2<-replicate(10000, {
+boot.lower.2<-nth(bootobject.1, (1000*(1-CI.2)))
+boot.upper.2<-nth(bootobject.1, (1000*CI.2))
+
+bootobject.2<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
@@ -46,7 +55,7 @@ bootobject.2<-replicate(10000, {
   
   for (i in y){
     #sample replicate
-    dif.start2<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,i]))
+    dif.start2<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=mal_sub[,i]))
     #edit dataframe
     names(dif.start2)<-c("Hap")
     dif.start2<-count(dif.start2,Hap)
@@ -63,16 +72,32 @@ bootobject.2<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
   dif<-dif.mid$R1-dif.mid$R2
 }
 )
+
+#find median difference
 boot.df[hap,2]<-median(bootobject.2)
 
+#determine significance based on CI values
+if (is.na(boot.lower)) {
+  if (is.na(boot.upper)) {boot.df[hap,14] <- "NA"} else if (median(bootobject.2) > boot.upper) {boot.df[hap,14] <- "*"} else {boot.df[hap,14] <- "ns"}
+} else if (median(bootobject.2) < boot.lower) {boot.df[hap,14] <- "*"} else {
+  if (is.na(boot.upper)) {boot.df[hap,14] <- "NA"} else if (median(bootobject.2) > boot.upper) {boot.df[hap,14] <- "*"} else {boot.df[hap,14] <- "ns"}
+}
+
+if (is.na(boot.lower.2)) {
+  if (is.na(boot.upper.2)) {boot.df[hap,16] <- "NA"} else if (median(bootobject.2) > boot.upper.2) {boot.df[hap,16] <- "*"} else {boot.df[hap,16] <- "ns"}
+} else if (median(bootobject.2) < boot.lower.2) {boot.df[hap,16] <- "*"} else {
+  if (is.na(boot.upper.2)) {boot.df[hap,16] <- "NA"} else if (median(bootobject.2) > boot.upper.2) {boot.df[hap,16] <- "*"} else {boot.df[hap,16] <- "ns"}
+}
+
 #bootstrap to CI and median for replicate 2
-bootobject.1<-replicate(10000, {
+avg.df<-as.data.frame((mal_sub[,6]+mal_sub[,8])/2)
+bootobject.1<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
@@ -81,7 +106,7 @@ bootobject.1<-replicate(10000, {
   
   for (i in z){
     #sample replicate
-    dif.start<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,sample(z, size=1, replace=TRUE)]))
+    dif.start<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=avg.df[,1]))
     #edit dataframe
     names(dif.start)<-c("Hap")
     dif.start<-count(dif.start,Hap)
@@ -98,19 +123,22 @@ bootobject.1<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
   dif<-dif.mid$R1-dif.mid$R2
 }
 )
 bootobject.1<-sort(bootobject.1, decreasing=FALSE)
-boot.lower<-nth(bootobject.1, 500)
-boot.df[hap,5]<-boot.lower
-boot.upper<-nth(bootobject.1, 9500)
-boot.df[hap,6]<-boot.upper
+boot.lower<-nth(bootobject.1, (1000*(1-CI)))
+boot.df[hap,6]<-boot.lower
+boot.upper<-nth(bootobject.1, (1000*CI))
+boot.df[hap,7]<-boot.upper
 
-bootobject.2<-replicate(10000, {
+boot.lower.2<-nth(bootobject.1, (1000*(1-CI.2)))
+boot.upper.2<-nth(bootobject.1, (1000*CI.2))
+
+bootobject.2<-replicate(1000, {
   
   dif.df<-as.data.frame(matrix(nrow=hap_num, ncol=1)) 
   names(dif.df)<-c("Hap")
@@ -119,7 +147,7 @@ bootobject.2<-replicate(10000, {
   
   for (i in z){
     #sample replicate
-    dif.start2<-as.data.frame(sample(c(1:hap_num), size=10000, replace=TRUE, prob=mal_sub[,i]))
+    dif.start2<-as.data.frame(sample(c(1:hap_num), size=1000, replace=TRUE, prob=mal_sub[,i]))
     #edit dataframe
     names(dif.start2)<-c("Hap")
     dif.start2<-count(dif.start2,Hap)
@@ -136,10 +164,22 @@ bootobject.2<-replicate(10000, {
   dif.mid<-slice(dif.mid, hap)
   
   #transform to proportions
-  dif.mid[,2:3]<-dif.mid[,2:3]/10000
+  dif.mid[,2:3]<-dif.mid[,2:3]/1000
   
   #add difference column
-  dif<-dif.mid$R1-dif.mid$R2
+  dif<-dif.mid$R2-dif.mid$R1
 }
 )
 boot.df[hap,5]<-median(bootobject.2)
+
+if (is.na(boot.lower)) {
+  if (is.na(boot.upper)) {boot.df[hap,15] <- "NA"} else if (median(bootobject.2) > boot.upper) {boot.df[hap,15] <- "*"} else {boot.df[hap,15] <- "ns"}
+} else if (median(bootobject.2) < boot.lower) {boot.df[hap,15] <- "*"} else {
+  if (is.na(boot.upper)) {boot.df[hap,15] <- "NA"} else if (median(bootobject.2) > boot.upper) {boot.df[hap,15] <- "*"} else {boot.df[hap,15] <- "ns"}
+}
+
+if (is.na(boot.lower.2)) {
+  if (is.na(boot.upper.2)) {boot.df[hap,17] <- "NA"} else if (median(bootobject.2) > boot.upper.2) {boot.df[hap,17] <- "*"} else {boot.df[hap,17] <- "ns"}
+} else if (median(bootobject.2) < boot.lower.2) {boot.df[hap,17] <- "*"} else {
+  if (is.na(boot.upper.2)) {boot.df[hap,17] <- "NA"} else if (median(bootobject.2) > boot.upper.2) {boot.df[hap,17] <- "*"} else {boot.df[hap,17] <- "ns"}
+}
